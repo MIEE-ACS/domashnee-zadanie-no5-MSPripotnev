6353879a74b69d79b9a0832f38f6a7b923365d02
MSPripotnev <mikhail.pripotnev@yandex.ru>
2020-11-07 04:38:18 +0300

    Homework5
    Домашнее задание №5 выполнено.

---
+++ Homework5/Employee.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework5
+{
+    internal class Employee
+    {
+        int id;
+        float salary;
+        /// <summary>
+        /// Идентификационный номер работника.
+        /// </summary>
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Номер должен быть положительным!");
+                id = value;
+            }
+        }
+        /// <summary>
+        /// Зарплата работника.
+        /// </summary>
+        public float Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new Exception("Зарплата не может быть отрицательной!");
+                salary = value;
+            }
+        }
+        /// <summary>
+        /// Имя работника.
+        /// </summary>
+        public string Name { get; set; } = "Ivan";
+        /// <summary>
+        /// Должность работника.
+        /// </summary>
+        public string Post { get; set; } = "Worker";
+        /// <summary>
+        /// Отдел работника.
+        /// </summary>
+        public string Department { get; set; } = "Earth";
+
+        #region Construct
+        public Employee()
+        {
+            ID = 1;
+            Salary = 0;
+            Name = "Ivan";
+            Post = "Worker";
+            Department = "Earth";
+        }
+        public Employee(int Id, float sal)
+        {
+            ID = Id;
+            Salary = sal;
+            Name = "Ivan";
+            Post = "Worker";
+            Department = "Earth";
+        }
+        /// <summary>
+        /// Расширенный конструктор работника.
+        /// </summary>
+        /// <param name="Id">Идентификационный номер.</param>
+        /// <param name="sal">Зарплата работника.</param>
+        /// <param name="name">Имя работника.</param>
+        /// <param name="post">Должность.</param>
+        /// <param name="department">Отдел работника.</param>
+        public Employee(int Id, float sal, string name, string post, string department)
+        {
+            ID = Id;
+            Salary = sal;
+            Name = name ?? "Ivan";
+            Post = post ?? "Worker";
+            Department = department ?? "Earth";
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return "ID: " + ID + "\n" +
+                "Name: " + Name + "\n" +
+                "Department: " + Department + "\n" +
+                "Post: " + Post + "\n" +
+                "Salary: " + Salary + "\n";
+        }
+    }
+}
---
+++ Homework5/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework5
+{
+    public partial class EmployersForm : Form
+    {
+        List<Employee> employers;
+        List<Employee> Employers
+        {
+            get
+            {
+                return employers;
+            }
+            set
+            {
+                employers = value;
+                EmployersLV.Items.Clear();
+                foreach (Employee el in Employers)
+                    EmployersLV.Items.Add(el.ID.ToString(), 0);
+                EmployersLV.Sort();
+                EmployersPG.SelectedObject = null;
+            }
+        }
+        public EmployersForm()
+        {
+            InitializeComponent();
+            Employers = new List<Employee>();
+            BasicFounders();
+        }
+
+        private void BasicFounders()
+        {
+            Employee Ivan1 = new Employee(1, 1000F, "Ivan", "Knyaz", "Moscow");
+            Employee Ivan4 = new Employee(4, 250F, "Ivan \"Grozniy\"", "Tsar", "Rus");
+            Employee Petr1 = new Employee(2, 1703.21F, "Petr", "Emperor", "Russian Empire");
+            Employee Lenin = new Employee(3, 1917.24F, "Vladimir", "Chairman", "USSR");
+            Employee Stalin = new Employee(5, 500.45F, "Joseph", "Vozd", "USSR");
+            Employee Breznev = new Employee(10, 50000.25F, "Leonid", "Secretary-general", "USSR");
+            Employee Gorbachev = new Employee(11, 90000.49999F, "Mikhail", "Perzident", "USSR");
+            Employee Eltsin = new Employee(12, 100000.499999F, "Boris", "Prezident", "Russia");
+
+            Employee Ivan = new Employee(100, 5);
+            Employee Ivan_copy = new Employee();
+            Employee Ivan_copy_copy = new Employee();
+            Employee Ivan_copy_copy_copy = new Employee();
+            Ivan_copy.ID = 101; Ivan_copy_copy.ID = 102;
+            Ivan_copy_copy_copy.ID = 103;
+            Ivan_copy.Department = "Mars";
+            Ivan_copy_copy.Post = "Engineer";
+
+            Employee Max = new Employee(201, 10, "Max", "Hero", "Parmecia");
+            Employee Bowie = new Employee(202, 10, "Bowie", "Hero", "Granzeal");
+            Employee Chaz = new Employee(203, 5, "Chaz", "Guardian", "Algo");
+
+            Employers.Add(Ivan1);
+            Employers.Add(Ivan4);
+            Employers.Add(Petr1);
+            Employers.Add(Lenin);
+            Employers.Add(Stalin);
+            Employers.Add(Breznev);
+            Employers.Add(Gorbachev);
+            Employers.Add(Eltsin);
+            Employers.Add(Ivan);
+            Employers.Add(Ivan_copy);
+            Employers.Add(Ivan_copy_copy);
+            Employers.Add(Ivan_copy_copy_copy);
+            Employers.Add(Max);
+            Employers.Add(Bowie);
+            Employers.Add(Chaz);
+            Employers = Employers;
+        }
+
+        private void AddEmployeeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Employee employee = new Employee(
+                    (int)(Employers.Find(p => p.ID == idNUD.Value) == null ? idNUD.Value : throw new Exception("Сотрудник с таким идентификационным номером уже есть в базе данных!")),
+                    (float)salaryNUD.Value,
+                    NameTB.Text == "" ? null : NameTB.Text,
+                    PostTB.Text == "" ? null : PostTB.Text,
+                    DepartmentTB.Text == "" ? null : DepartmentTB.Text
+                );
+                Employers.Add(employee);
+                Employers = Employers;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DeleteEmployeeButton_Click(object sender, EventArgs e)
+        {
+            if (EmployersLV.SelectedItems.Count == 0) return;
+            foreach (ListViewItem el in EmployersLV.SelectedItems)
+                Employers.RemoveAll(p => p.ID.ToString() == el.Text);
+            Employers = Employers;
+        }
+
+        private void ChangeEmployeeButton_Click(object sender, EventArgs e)
+        {
+            if (EmployersLV.SelectedItems.Count < 1) return;
+            if (EmployersLV.SelectedItems.Count > 1)
+                MessageBox.Show("Пожалуйста, выберите только одного сотрудника!");
+            EmployersPG.SelectedObject = Employers.Find(p => 
+                p.ID.ToString() == EmployersLV.SelectedItems.Cast<ListViewItem>().ToArray()[0].Text);
+        }
+
+        private void MoreInfoButton_Click(object sender, EventArgs e)
+        {
+            if (EmployersLV.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Выберите хотя бы одного сотрудника!");
+                return;
+            }    
+            foreach (ListViewItem lvi in EmployersLV.SelectedItems)
+                MessageBox.Show(Employers.Find(p => p.ID.ToString() == lvi.Text).ToString(), "ID: " + lvi.Text);
+        }
+    }
+}
